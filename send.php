56f3fa2727a04636cdb2f132e6505b4905660ce8
Carlos Castellanos <89616377+castellanoscc@users.noreply.github.com>
2022-04-16 14:10:06 -0400

    Add files via upload

---
+++ send.php
@@ -0,0 +1,31 @@
+<?php  
+if (isset($_POST['vfirstName']) && isset($_POST['vlastName'])) {
+	include 'db_conn.php';
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$vfirstName = validate($_POST['vfirstName']);
+	$vlastName = validate($_POST['vlastName']);
+
+	if (empty($vlastName) || empty($vfirstName)) {
+		header("Location: experience.html");
+	}else {
+
+		$sql = "INSERT INTO test(vfirstName, vlastName) VALUES('$vfirstName', '$vlastName')";
+		$res = mysqli_query($conn, $sql);
+
+		if ($res) {
+			echo "Your message was sent successfully!";
+		}else {
+			echo "Your message could not be sent!";
+		}
+	}
+
+}else {
+	header("Location: index.html");
+}
