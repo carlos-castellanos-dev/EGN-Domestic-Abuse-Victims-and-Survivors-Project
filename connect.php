0366945e8ec0a420d121d7bc3fc032db48633bdd
rustyfred <89756659+rustyfred@users.noreply.github.com>
2022-04-16 17:02:34 -0400

    updated to connect.php

--- connect.php
+++ connect.php
@@ -1,34 +1,15 @@
-<?php
-function OpenCon()
- {
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "";
- $db = "test";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- 
- return $conn;
- }
- 
-function CloseCon($conn)
- {
- $conn -> close();
- }
-   
-?>
 
 <?php  
 
  $dbhost = "localhost";
  $dbuser = "root";
- $dbpass = "";
+ $dbpass = "tA5aWsE5mKbC";
  $db = "test";
 
- $conn = mysqli_connect($sname, $uname, $password, $db_name);
+ $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
 
  if (!$conn) {
-	echo "Connection failed!";
+	echo "Connection failed here!";
 	exit();
 }
 ?>
-
