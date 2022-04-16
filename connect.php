9375152e1ba31e8fd715aa4118e9d052afa63ad1
rustyfred <89756659+rustyfred@users.noreply.github.com>
2022-04-16 16:15:42 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -18,16 +18,3 @@ function CloseCon($conn)
 ?>
 
 
-<?php  
-
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "";
- $db = "test";
-
- $conn = mysqli_connect($sname, $uname, $password, $db_name);
-
- if (!$conn) {
-	echo "Connection failed!";
-	exit();
-}
