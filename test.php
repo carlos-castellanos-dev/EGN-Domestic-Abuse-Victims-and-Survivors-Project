3fafa83e672e03c7f244061826601b18512447b3
castellanoscc <castellanosc0520@gmail.com>
2022-04-16 05:25:19 -0400

    test

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "ls-3ddf96afc758a0f6cdeaa24ce90edcf1d293a292.cdmnpinl6gyw.us-east-1.rds.amazonaws.com";
+$username = "dbmasteruser";
+$password = "VDG+}8WMuM+[KM5TBNEJyVN98hSJ1R)Z";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
