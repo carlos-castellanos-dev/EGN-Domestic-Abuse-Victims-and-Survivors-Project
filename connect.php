4f1deeacb41eccc9dfd1d2882abbfc5dca506141
Carlos Castellanos <89616377+castellanoscc@users.noreply.github.com>
2022-04-16 14:10:21 -0400

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,48 +1,33 @@
 <?php
-    $server = "localhost" ;
-    $username = "root" ;
-    $password = "" ;
-    $dbname = "entry_form" ;
-    $vfirstName = $_POST['vfirstName'];
-    $vlastName = $_POST['vlastName'];
-    $vEmail = $_POST['vEmail'];
-    $vNumber = $_POST['vNumber'];
-    $cpfirstName = $_POST['cpfirstName'];
-    $cplastName = $_POST['cplastName'];
-    $cpEmail = $_POST['cpEmail'];
-    $cpNumber = $_POST['cpNumber'];
-    $date = $_POST['date'];
-    $listName = $_POST['listName'];
-    $experience = $_POST['experience'];
-    $outcome = $_POST['outcome'];
-    $other = $_POST['other'];
-
-    //Database Connection
-    $conn = new mysqli($server, $username, $password, $dbname);
-    if($conn-> connect_error){
-        die('Connection Failed :'.$conn->connect_error);
-    }else{
-        $stmt = $conn->prepare("insert into form(
-        vfirstName,
-        vlastName,
-        vEmail,
-        vNumber,
-        cpfirstName,
-        cplastName,
-        cpEmail,
-        cpNumber,
-        date,
-        listName,
-        experience,
-        outcome,
-        other)
-        values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
-        $stmt->bind_param("sssisssiissss",$vfirstName, $vlastName, $vEmail, $vNumber, $cpfirstName,             $cplastName, $cpEmail, $cpNumber, $date, $listName, $experience, $outcome, $other);
-        $stmt->execute();
-        echo "Submission Succesful...";
-        $stmt->close();
-        $conn->close();
-    }
-        
-
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "tA5aWsE5mKbC";
+ $db = "test";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+   
 ?>
+
+
+<?php  
+
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "tA5aWsE5mKbC";
+ $db = "test";
+
+ $conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+ if (!$conn) {
+	echo "Connection failed!";
+	exit();
+}
